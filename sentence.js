bf21fa61050ab714d6b3ee5a98c897607c10521c
gafar <cxlv2@00x010xc.local>
2021-12-26 22:37:35 -0800

    commenting of the code

--- sentence.js
+++ sentence.js
@@ -1,19 +1,17 @@
 //define str variable i.e sentence to be read
 const str = "i am writing an algorithm.";
-
+//function for counting the length of sentence
 let alphabet_count = (str) => str.length;
-// console.log(alphabet_count(str));
-
+//function for counting the number of words in sentence
 let word_count = (str) => str.split(" ").length;
-// console.log(word_count(str));
-
+//function for counting the amount of vowels present
 let vowel_count = (str) => str.match(/[aeiou]/gi).length;
-// console.log(vowel_count(str));
 
+// variable declaration
 let a = alphabet_count(str);
 let b = word_count(str);
 let c = vowel_count(str);
-
+//function to return all the functions
 const sentence_read = () => {
   return { a, b, c };
 };
